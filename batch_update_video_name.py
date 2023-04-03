e516d7b1b52cb31f8c4b3e7f0a8a549b6be7125e
Noodle <Noodlewar@yeah.net>
2023-04-03 10:20:23 +0800

    调整标题

--- batch_update_video_name.py
+++ batch_update_video_name.py
@@ -15,7 +15,7 @@ for file in files:
     if type != 'mp4':
         continue
     # 构造新文件名
-    new_name = os.path.join(path, '【知秋剧场】《仙尊归来当赘婿》第' + name + '集 #赘婿 #仙侠 #大陆 #言情.' + type)
+    new_name = os.path.join(path, '【知秋剧场】《仙尊归来当赘婿》EP' + name + ' #赘婿 #仙侠 #大陆 #言情.' + type)
     print(new_name)
     # 重命名文件
     os.rename(old_name, new_name)
